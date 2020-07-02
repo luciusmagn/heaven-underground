7a092519a0647b0cb4e2ccfddb5a9e2c96d89eb9
Lukáš Hozda <luk.hozda@gmail.com>
2020-07-02 22:38:49 +0200

    format and autofix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use coffee::graphics::{Color, Frame, Window, WindowSettings, Text, Font, Point, HorizontalAlignment, VerticalAlignment};
+use coffee::graphics::{
+	Color, Frame, Window, WindowSettings, Text, Font, Point, HorizontalAlignment,
+	VerticalAlignment,
+};
 use coffee::load::Task;
 use coffee::input::KeyboardAndMouse;
 use coffee::input::keyboard::KeyCode;
@@ -15,20 +18,20 @@ pub mod state;
 //use state::MyGame;
 
 fn main() -> Result<()> {
-    MyGame::run(WindowSettings {
-        title: String::from("A caffeinated game"),
-        size: (1280, 1024),
-        resizable: true,
-        fullscreen: false,
-        maximized: false,
-    })
+	MyGame::run(WindowSettings {
+		title:      String::from("A caffeinated game"),
+		size:       (1280, 1024),
+		resizable:  true,
+		fullscreen: false,
+		maximized:  false,
+	})
 }
 
 struct MyGame {
-    // Your game state and assets go here...
-    blinker: bool,
-    tick_count: u64,
-    text_buffer: String,
+	// Your game state and assets go here...
+	blinker:     bool,
+	tick_count:  u64,
+	text_buffer: String,
 }
 
 impl MyGame {
@@ -39,58 +42,73 @@ impl MyGame {
 
 fn make_text<'a>(src: &'a str, pos: Point, size: f32) -> Text<'a> {
 	Text {
-		content: src,
-		position: pos,
-		size: size,
-		color: Color::WHITE,
-		bounds: (600.0, 600.0),
+		content:              src,
+		position:             pos,
+		size,
+		color:                Color::WHITE,
+		bounds:               (600.0, 600.0),
 		horizontal_alignment: HorizontalAlignment::Left,
-		vertical_alignment: VerticalAlignment::Top,
+		vertical_alignment:   VerticalAlignment::Top,
 	}
 }
 
 impl Game for MyGame {
-    type Input = KeyboardAndMouse; // No input data
-    type LoadingScreen = (); // No loading screen
+	type Input = KeyboardAndMouse;
+	// No input data
+	type LoadingScreen = ();
 
-    fn load(_window: &Window) -> Task<MyGame> {
-        Task::succeed(|| MyGame { blinker: false, tick_count: 0, text_buffer: String::new() })
-    }
+	// No loading screen
 
-    fn interact(&mut self, input: &mut Self::Input, _window: &mut Window) {
-	    let kb = input.keyboard();
+	fn load(_window: &Window) -> Task<MyGame> {
+		Task::succeed(|| MyGame {
+			blinker:     false,
+			tick_count:  0,
+			text_buffer: String::new(),
+		})
+	}
 
-	    let input_string = kb.released_keys.iter()
-	    	.map(|x| x.to_printable())
-	    	.collect::<String>();
+	fn interact(&mut self, input: &mut Self::Input, _window: &mut Window) {
+		let kb = input.keyboard();
+
+		let input_string =
+			kb.released_keys.iter().map(|x| x.to_printable()).collect::<String>();
 
 		self.text_buffer = format!("{}{}", self.text_buffer, input_string);
 
 		if kb.released_keys.contains(&KeyCode::Back) {
-			self.text_buffer = self.text_buffer[..cmp::max(self.text_buffer.len()-1, 0)].to_string();
+			self.text_buffer =
+				self.text_buffer[..cmp::max(self.text_buffer.len() - 1, 0)].to_string();
 		}
-    }
+	}
 
-    fn update(&mut self, _window: &Window) {
-	    if self.tick_count % 60 < 30 {
-		    self.blinker = true;
-	    } else {
-		    self.blinker = false;
-	    }
-	    self.tick();
-    }
+	fn update(&mut self, _window: &Window) {
+		if self.tick_count % 60 < 30 {
+			self.blinker = true;
+		} else {
+			self.blinker = false;
+		}
+		self.tick();
+	}
 
-    fn draw(&mut self, frame: &mut Frame, _timer: &Timer) {
-        frame.clear(Color::BLACK);
+	fn draw(&mut self, frame: &mut Frame, _timer: &Timer) {
+		frame.clear(Color::BLACK);
 
 		let mut f = Font::from_bytes(frame.gpu(), &PROFONT).unwrap();
 
 		match self.blinker {
-			true => f.add(make_text(&format!("{}_", self.text_buffer), Point::new(100.0, 100.0), 60.0)),
-			false => f.add(make_text(&format!("{}", self.text_buffer), Point::new(100.0, 100.0), 60.0)),
+			true => f.add(make_text(
+				&format!("{}_", self.text_buffer),
+				Point::new(100.0, 100.0),
+				60.0,
+			)),
+			false => f.add(make_text(
+				&format!("{}", self.text_buffer),
+				Point::new(100.0, 100.0),
+				60.0,
+			)),
 		}
 
-        let mut target = frame.as_target();
+		let mut target = frame.as_target();
 		f.draw(&mut target);
-    }
+	}
 }
--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,4 @@
-use coffee::{Game, Timer};
+use coffee::{Timer};
 use coffee::graphics::{Frame, Window};
 use coffee::input::KeyboardAndMouse;
 use either::Either;
@@ -17,22 +17,16 @@ pub enum Direction {
 
 pub enum Action {
 	ChangeScreen,
-	MutateState(Box<dyn Fn(&mut Heaven) -> ()>)
+	MutateState(Box<dyn Fn(&mut Heaven) -> ()>),
 }
 
 pub enum Screen {
-	Menu {
-    	buttons: Vec<(String, Action)>
-	},
-	About {
-		scrolling_dir: Direction,
-	},
+	Menu { buttons: Vec<(String, Action)> },
+	About { scrolling_dir: Direction },
 	ReadStory,
 	Options, //?
 	Quit,
-	Play {
-		buttons: Vec<(String, Action)>
-	},
+	Play { buttons: Vec<(String, Action)> },
 	PlayCutscene(String),
 	PlayMinigame(String),
 }
@@ -40,34 +34,33 @@ pub enum Screen {
 pub type Node = Either<Arc<Event>, Action>;
 
 pub enum Event {
-    TimeScreen(Node),
+	TimeScreen(Node),
 	Text(Node),
 	MultipleChoice(Vec<Node>),
 	Choice(Node, Node),
-    GameOver(String),
-    Cutscene(String, Node),
-    Minigame(String, Box<dyn Fn(String) -> Node>),
-    End,
+	GameOver(String),
+	Cutscene(String, Node),
+	Minigame(String, Box<dyn Fn(String) -> Node>),
+	End,
 }
 
 pub struct Tree {
-	pub path: Vec<usize>,
+	pub path:             Vec<usize>,
 	pub minigame_results: Vec<String>,
-	pub start: Event,
+	pub start:            Event,
 }
 
 pub trait Minigame {
-    fn update(&mut self, window: &Window);
-    fn draw(&mut self, frame: &mut Frame, timer: &Timer);
-    fn interact(&mut self, input: &mut KeyboardAndMouse, window: &mut Window);
+	fn update(&mut self, window: &Window);
+	fn draw(&mut self, frame: &mut Frame, timer: &Timer);
+	fn interact(&mut self, input: &mut KeyboardAndMouse, window: &mut Window);
 }
 
 pub struct Heaven {
-	pub screen: Screen,
+	pub screen:     Screen,
 	pub tick_count: u64,
-	pub fonts: HashMap<&'static str, Vec<u8>>,
-	pub sprites: HashMap<&'static str, Vec<u8>>,
+	pub fonts:      HashMap<&'static str, Vec<u8>>,
+	pub sprites:    HashMap<&'static str, Vec<u8>>,
 	pub event_tree: Tree,
-	pub minigames: HashMap<&'static str, Box<dyn Minigame>>,
+	pub minigames:  HashMap<&'static str, Box<dyn Minigame>>,
 }
-
