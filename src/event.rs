d19ea50385830e4ca4eab5ee21eb0a5657a99489
Lukáš Hozda <luk.hozda@gmail.com>
2020-07-09 12:13:56 +0200

    stubs

--- src/event.rs
+++ src/event.rs
@@ -1,8 +1,16 @@
+use coffee::{
+	Timer, Game,
+	graphics::{
+		Frame,
+		Window,
+	},
+	input::KeyboardAndMouse,
+};
 use either::Either;
 
 use std::sync::Arc;
 
-use crate::state::Action;
+use crate::state::{Action, Heaven};
 
 pub type Node = Either<Arc<Event>, Action>;
 
@@ -16,6 +24,21 @@ pub enum Event {
 	Minigame(String, Box<dyn Fn(String) -> Node>),
 	ChapterDelimiter,
 	End,
+}
+
+#[allow(unused_variables)]
+impl Event {
+	pub fn render(heaven: &mut Heaven, frame: &mut Frame, _timer: &Timer) {
+
+	}
+
+	pub fn interact(heaven: &mut Heaven, input: &mut KeyboardAndMouse, window: &mut Window) -> Result<(), Box<dyn std::error::Error + 'static>> {
+		Ok(())
+	}
+
+	pub fn update(&mut self, window: &Window) {
+
+	}
 }
 
 pub struct Tree {
